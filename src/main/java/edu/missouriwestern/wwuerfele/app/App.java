69cc6b08fc678905092c6bacc475d56b6a1b3200
Wuerfele-Wyatt <99354113+Wuerfele-Wyatt@users.noreply.github.com>
2025-02-18 16:15:01 -0600

    Initial commit

---
+++ src/main/java/edu/missouriwestern/wwuerfele/app/App.java
@@ -0,0 +1,9 @@
+package edu.missouriwestern.wwuerfele.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        final String USER_NAME = "csc";
+        final String PASSWORD = "sandy2714288";
+    }
+}
