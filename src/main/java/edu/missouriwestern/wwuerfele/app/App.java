dbf91103203de1487e5d5474f352725894c1a8a3
Wuerfele-Wyatt <99354113+Wuerfele-Wyatt@users.noreply.github.com>
2025-02-18 16:44:16 -0600

    Attach dummyCredentials.txt

--- src/main/java/edu/missouriwestern/wwuerfele/app/App.java
+++ src/main/java/edu/missouriwestern/wwuerfele/app/App.java
@@ -2,8 +2,7 @@ package edu.missouriwestern.wwuerfele.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        final String USER_NAME = "csc";
-        final String PASSWORD = "sandy2714288";
+        System.out.println("Done");
+
     }
 }
