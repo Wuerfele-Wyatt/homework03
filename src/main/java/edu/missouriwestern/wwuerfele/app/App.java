0a770f457fc5edafa59a9b7d3ad04486fa9fb7b3
Wuerfele-Wyatt <99354113+Wuerfele-Wyatt@users.noreply.github.com>
2025-02-18 18:12:38 -0600

    Started Credentials method in App

--- src/main/java/edu/missouriwestern/wwuerfele/app/App.java
+++ src/main/java/edu/missouriwestern/wwuerfele/app/App.java
@@ -4,5 +4,18 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Done");
 
-    }
-}
+    }//end of main
+
+    private static Credentials getCredentials(String[] args) throws EmptyCommandLineException {
+        Credentials credentials = new Credentials("???", "!!!");
+        if(args.length < 1){
+            throw new EmptyCommandLineException("No file name was entered in command line");
+        }else {
+            String fileName = args[0].trim();
+            System.out.println("The file name is " + fileName);
+        }
+
+        return credentials;
+    }//end of Credentials
+}//end of App
+
