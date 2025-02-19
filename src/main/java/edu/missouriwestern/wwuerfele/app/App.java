be82d579d9d669b9cd9ed26496c8d4a20bbd5d7b
Wuerfele-Wyatt <99354113+Wuerfele-Wyatt@users.noreply.github.com>
2025-02-19 11:26:54 -0600

    Update getCredentials

--- src/main/java/edu/missouriwestern/wwuerfele/app/App.java
+++ src/main/java/edu/missouriwestern/wwuerfele/app/App.java
@@ -1,18 +1,44 @@
 package edu.missouriwestern.wwuerfele.app;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
 public class App {
     public static void main(String[] args) {
+        
+
+
         System.out.println("Done");
 
     }//end of main
 
-    private static Credentials getCredentials(String[] args) throws EmptyCommandLineException {
+    /**
+     * The credentials file must be a single line with the username
+     * and password separated by ':'
+     *
+     * This takes the arg list and reads the file. It then attempts
+     * to read the file
+     * @param args -- This should be the args array from main()
+     * @return -- Returns a credential. If credential file was flawed then Username: ??? Password: !!!
+     * @throws EmptyCommandLineException
+     * @throws IOException
+     */
+
+    private static Credentials getCredentials(String[] args) throws EmptyCommandLineException, IOException {
         Credentials credentials = new Credentials("???", "!!!");
         if(args.length < 1){
             throw new EmptyCommandLineException("No file name was entered in command line");
         }else {
             String fileName = args[0].trim();
             System.out.println("The file name is " + fileName);
+            String line = Files.readString(new File(fileName).toPath(), StandardCharsets.UTF_8);
+            String[] parts = line.split(":");
+            if(parts.length != 2){
+                credentials.setUsername(parts[0]);
+                credentials.setPassword(parts[1]);
+            }
         }
 
         return credentials;
